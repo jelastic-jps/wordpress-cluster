315bf88a86bdff41b4b8be649a62729623bc96d5
gs <gs@jelastic.com>
2017-03-24 15:19:29 +0000

    Move to master branch

---
+++ scripts/addTriggers.js
@@ -0,0 +1,15 @@
+var APPID = getParam("TARGET_APPID");
+
+oQoutas = jelastic.billing.account.GetQuotas(APPID, session, ["environment.maxsamenodescount"]);
+
+if (oQoutas.result != 0) {
+    return oQoutas;
+}
+
+oRespTurnOn = jelastic.env.trigger.AddTrigger(APPID, session,'{"isEnabled":true,"name":"hs-add-nginx","nodeGroup":"cp","period":1,"condition":{"type":"GREATER","value":70,"resourceType":"CPU","valueType":"PERCENTAGES"},"actions":[{"type":"ADD_NODE","customData":{"limit":' + oQoutas.array[0].value + ',"count":1,"notify":true}}]}');
+
+if (oRespTurnOn.result != 0) {
+    return oRespTurnOn;
+}
+
+return jelastic.env.trigger.AddTrigger(APPID, session,'{"isEnabled":true,"name":"hs-add-nginx","nodeGroup":"cp","period":5,"condition":{"type":"LESS","value":20,"resourceType":"CPU","valueType":"PERCENTAGES"},"actions":[{"type":"REMOVE_NODE","customData":{"limit":2,"count":1,"notify":true}}]}');
