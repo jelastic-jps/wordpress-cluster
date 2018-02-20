7256db014a1a201b877fc9657e41c14a54598a00
dzotic9 <dzotic9@gmail.com>
2018-02-20 12:43:33 +0200

    Update addTriggers.js

--- scripts/addTriggers.js
+++ scripts/addTriggers.js
@@ -1,4 +1,5 @@
-var APPID = getParam("TARGET_APPID");
+var APPID = getParam("TARGET_APPID"),
+    nLimitQuota;
 
 oQoutas = jelastic.billing.account.GetQuotas(APPID, session, ["environment.maxsamenodescount"]);
 
@@ -6,10 +7,12 @@ if (oQoutas.result != 0) {
     return oQoutas;
 }
 
-oRespTurnOn = jelastic.env.trigger.AddTrigger(APPID, session,'{"isEnabled":true,"name":"hs-add-nginx","nodeGroup":"cp","period":1,"condition":{"type":"GREATER","value":70,"resourceType":"CPU","valueType":"PERCENTAGES"},"actions":[{"type":"ADD_NODE","customData":{"limit":' + oQoutas.array[0].value + ',"count":1,"notify":true}}]}');
+nLimitQuota = oQoutas.array[0].value;
+
+oRespTurnOn = jelastic.env.trigger.AddTrigger(APPID, session,'{"isEnabled":true,"name":"hs-add-nginx","nodeGroup":"cp","period":1,"condition":{"type":"GREATER","value":70,"resourceType":"CPU","valueType":"PERCENTAGES"},"actions":[{"type":"ADD_NODE","customData":{"limit":' + nLimitQuota + ',"count":1,"notify":true}}]}');
 
 if (oRespTurnOn.result != 0) {
     return oRespTurnOn;
 }
 
-return jelastic.env.trigger.AddTrigger(APPID, session,'{"isEnabled":true,"name":"hs-add-nginx","nodeGroup":"cp","period":5,"condition":{"type":"LESS","value":20,"resourceType":"CPU","valueType":"PERCENTAGES"},"actions":[{"type":"REMOVE_NODE","customData":{"limit":2,"count":1,"notify":true}}]}');
+return jelastic.env.trigger.AddTrigger(APPID, session,'{"isEnabled":true,"name":"hs-add-nginx","nodeGroup":"cp","period":5,"condition":{"type":"LESS","value":20,"resourceType":"CPU","valueType":"PERCENTAGES"},"actions":[{"type":"REMOVE_NODE","customData":{"limit":' + (nLimitQuota - 1 || 1) + ',"count":1,"notify":true}}]}');
