2d8a6817e1ec92662bf0c38c7ffc25534b6116d8
unigeocloud <unigeocloud@gmail.com>
2019-09-13 13:53:59 +0300

    Update beforeinstall.js

--- scripts/beforeinstall.js
+++ scripts/beforeinstall.js
@@ -1,18 +1,18 @@
 var resp = {
   result: 0,
   ssl: !!jelastic.billing.account.GetQuotas('environment.jelasticssl.enabled').array[0].value,
-    "nodes": [{
-        "nodeType": "mariadb-dockerized",
-        "tag": "10.3.16",
-        "count": 1,
-        "cloudlets": 16,
-        "nodeGroup": "sqldb",
-        "displayName": "Galera cluster",
-        "restartDelay": 5
+    nodes: [{
+        nodeType: "mariadb-dockerized",
+        tag: "10.3.16",
+        count: 1,
+        cloudlets: 16,
+        nodeGroup: "sqldb",
+        displayName: "Galera cluster",
+        restartDelay: 5
     }, {
-        "nodeType": "storage",
-        "cloudlets": 8,
-        "nodeGroup": "storage",
-        "displayName": "Storage"
+        nodeType: "storage",
+        cloudlets: 8,
+        nodeGroup: "storage",
+        displayName: "Storage"
     }]
 }
