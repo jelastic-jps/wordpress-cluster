d5227885acf793b17042130a254254fac90f6f1c
sych74 <sych74@gmail.com>
2019-10-16 11:01:35 +0300

    Update settings.js

--- scripts/settings.js
+++ scripts/settings.js
@@ -130,6 +130,8 @@ if (group.groupType == 'trial') {
                     },  {
                         "type": "tooltip",
                         "height": 24,
+                        "value": true,
+                        "name": "waf",
                         "text": "Protect web sites with <a href='https://www.litespeedtech.com/support/wiki/doku.php/litespeed_wiki:waf'>LiteSpeed built-in WAF</a> based on Free ModSecurity Rules from Comodo",
                         "margins": "0 10 0 0"
                     }, {
