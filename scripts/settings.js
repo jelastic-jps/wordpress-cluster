9736b11f8c7f72b37d7bfde229ad4ba214783476
sych74 <sych74@gmail.com>
2019-10-31 10:29:40 +0200

    Fixed disabling CDN/LS for trial

--- scripts/settings.js
+++ scripts/settings.js
@@ -4,7 +4,7 @@ import com.hivext.api.core.utils.Transport;
 
 var cdnAppid = "c05ffa5b45628a2a0c95467ebca8a0b4";
 var lsAppid = "9e6afcf310004ac84060f90ff41a5aba";
-var baseUrl = "https://raw.githubusercontent.com/sych74/wordpress-cluster/master";
+var baseUrl = "https://raw.githubusercontent.com/jelastic-jps/wordpress-cluster/master";
 var cdnText = "Install Lightning-Fast Premium CDN with 130+ PoPs",
     sslText = "Install Let's Encrypt SSL with Auto-Renewal";
     lsText = "Install LiteSpeed High-Performance Web Server";
@@ -12,6 +12,8 @@ var cdnText = "Install Lightning-Fast Premium CDN with 130+ PoPs",
     dbText = "Install MariaDB Galera Cluster";
     wafText = "Web Application Firewall";
 var group = jelastic.billing.account.GetAccount(appid, session);
+var isCDN = jelastic.dev.apps.GetApp(cdnAppid);
+var isLS = jelastic.dev.apps.GetApp(lsAppid);
 
 var url = baseUrl + "/configs/settings.yaml";
 var settings = toNative(new Yaml().load(new Transport().get(url)));
@@ -24,26 +26,32 @@ if (group.groupType == 'trial') {
       "height": 30,
       "hideLabel": true,
       "markup": "Not available for " + group.groupType + " account. Please upgrade your account."
-    }, {
-        "type": "compositefield",
-        "hideLabel": true,
-        "pack": "left",
-        "itemCls": "deploy-manager-grid",
-        "cls": "x-grid3-row-unselected",
-        "items": [{
-            "type": "spacer",
-            "width": 4
-        }, {
-            "type": "displayfield",
-            "cls": "x-grid3-row-checker x-item-disabled",
-            "width": 30,
-            "height": 20
-        }, {
-            "type": "displayfield",
-            "cls": "x-item-disabled",
-            "value": lsText
-        }]
-    }, {
+    })
+ 
+    if (isLS.result == 0 || isLS.result == Response.PERMISSION_DENIED) {
+        settings.fields.push({
+            "type": "compositefield",
+            "hideLabel": true,
+            "pack": "left",
+            "itemCls": "deploy-manager-grid",
+            "cls": "x-grid3-row-unselected",
+            "items": [{
+                "type": "spacer",
+                "width": 4
+            }, {
+                "type": "displayfield",
+                "cls": "x-grid3-row-checker x-item-disabled",
+                "width": 30,
+                "height": 20
+            }, {
+                "type": "displayfield",
+                "cls": "x-item-disabled",
+                "value": lsText
+            }]
+        });
+    }
+
+    settings.fields.push({
         "type": "compositefield",
         "hideLabel": true,
         "pack": "left",
@@ -62,26 +70,32 @@ if (group.groupType == 'trial') {
             "cls": "x-item-disabled",
             "value": dbText
         }]
-    }, {
-        "type": "compositefield",
-        "hideLabel": true,
-        "pack": "left",
-        "itemCls": "deploy-manager-grid",
-        "cls": "x-grid3-row-unselected",
-        "items": [{
-            "type": "spacer",
-            "width": 4
-        }, {
-            "type": "displayfield",
-            "cls": "x-grid3-row-checker x-item-disabled",
-            "width": 30,
-            "height": 20
-        }, {
-            "type": "displayfield",
-            "cls": "x-item-disabled",
-            "value": cdnText
-        }]
-    }, {
+    });
+ 
+    if (isCDN.result == 0 || isCDN.result == Response.PERMISSION_DENIED) {
+        settings.fields.push({
+            "type": "compositefield",
+            "hideLabel": true,
+            "pack": "left",
+            "itemCls": "deploy-manager-grid",
+            "cls": "x-grid3-row-unselected",
+            "items": [{
+                "type": "spacer",
+                "width": 4
+            }, {
+                "type": "displayfield",
+                "cls": "x-grid3-row-checker x-item-disabled",
+                "width": 30,
+                "height": 20
+            }, {
+                "type": "displayfield",
+                "cls": "x-item-disabled",
+                "value": cdnText
+            }]
+        });
+    }
+    
+    settings.fields.push({
         "type": "compositefield",
         "hideLabel": true,
         "pack": "left",
@@ -100,16 +114,18 @@ if (group.groupType == 'trial') {
             "cls": "x-item-disabled",
             "value": sslText
         }]
-    }, {
+    });
+
+    settings.fields.push({
         type: "checkbox",
         name: "mu-addon",
         caption: muText,
         value: false
 
     });
+    
 } else {
 
-    var isLS = jelastic.dev.apps.GetApp(lsAppid);
     if (isLS.result == 0 || isLS.result == Response.PERMISSION_DENIED) {
         settings.fields.push({
             type: "checkbox",
@@ -159,7 +175,6 @@ if (group.groupType == 'trial') {
         tooltip: "<h3>Requirements for All Tables:</h3> * run on InnoDB storage engine <p>* have a primary key</p>Read more about <a href='https://mariadb.com/kb/en/library/mariadb-galera-cluster-known-limitations/'>limitations</a>"
     });
    
-    var isCDN = jelastic.dev.apps.GetApp(cdnAppid);
     if (isCDN.result == 0 || isCDN.result == Response.PERMISSION_DENIED) {
         settings.fields.push({
             type: "checkbox",
