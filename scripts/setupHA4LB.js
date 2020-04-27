67ea50e50d462b8b60b0036dea954474117fbca6
HENADII SYCHEVSKYI <SGI@HENADIIs-MacBook-Pro-4.local>
2020-04-27 16:47:01 +0300

    Setup HA for LB

--- scripts/setupHA4LB.js
+++
@@ -1,8 +0,0 @@
-var resp = jelastic.env.control.GetEnvInfo(targetEnv, session);
-if (resp.result != 0) return resp;
-var nodes = [];
-for (var i = 0, n = resp.nodes; i < n.length; i++)
-  n[i].nodeGroup == nodeGroup ? nodes.push('node' + n[i].address) : 0
-resp = {result:0, onAfterReturn: {}};
-resp.onAfterReturn['cmd['+ nodeGroup +']'] = 'echo '+ nodes.join(',');
-return resp;
