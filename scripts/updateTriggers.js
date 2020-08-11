7ac156929e17520130633ffeb202d57a0985ad63
alexey.lazarenko <dzotic9@gmail.com>
2020-08-11 11:24:45 +0300

    JE-54643 (cherry picked from commit 284324b2bec51cb586b7399ee8f0ff54d12f32db)

--- scripts/updateTriggers.js
+++ scripts/updateTriggers.js
@@ -1,8 +1,11 @@
-var triggerNames = ["scale-up", "scale-down"],
+var SCALE = "scale-",
     DOWN = "down",
     UP = "up",
-    actions = ['ADD_NODE', 'REMOVE_NODE'],
-    cpCount = getParam('cpCount'),
+    scaleUp = SCALE + UP,
+    scaleDown = SCALE + DOWN,
+    addNode = "ADD_NODE",
+    removeNode = "REMOVE_NODE",
+    count = getParam('count'),
     ENV_NAME = "${env.name}",
     triggersToEdit = [],
     triggerActions,
@@ -10,26 +13,24 @@ var triggerNames = ["scale-up", "scale-down"],
     triggers,
     upLimit,
     resp;
-    
 
-resp = jelastic.environment.trigger.GetTriggers(ENV_NAME, session, actions[0] + ";" + actions[1]);
+resp = jelastic.environment.trigger.GetTriggers(ENV_NAME, session, addNode + ";" + removeNode);
 if (resp.result != 0) return resp;
 
 triggers = resp.array;
 
 for (var i = 0, n = triggers.length; i < n; i++) {
-
-    if (triggerNames.indexOf(String(triggers[i].name)) != -1) {
+    if ([scaleUp, scaleDown].indexOf(String(triggers[i].name)) != -1) {
         triggerActions = triggers[i].actions;
 
         for (var l = 0, m = triggerActions.length; l < m; l++) {
             customData = triggerActions[l].customData;
             
             if (customData && customData.limit) {
-                if (triggers[i].name == triggerNames[0]) upLimit = customData.limit;
+                if (triggers[i].name == scaleUp) upLimit = customData.limit;
 
-                if ((triggerActions[l].type == actions[0] && customData.limit < cpCount) || triggerActions[l].type == actions[1]) {
-                    triggers[i].actions[l].customData.limit = cpCount;
+                if ((triggerActions[l].type == addNode && customData.limit < count) || triggerActions[l].type == removeNode) {
+                    triggers[i].actions[l].customData.limit = count;
 
                     triggersToEdit.push({
                         scale: triggers[i].name.indexOf(UP) != -1 ? UP : DOWN,
@@ -44,7 +45,7 @@ for (var i = 0, n = triggers.length; i < n; i++) {
 for (var i = 0, n = triggersToEdit.length; i < n; i++) {
     trigger = triggersToEdit[i].trigger;
     
-    if (triggersToEdit[i].scale == DOWN && Number(trigger.actions[0].customData.limit) >= upLimit) trigger.actions[0].customData.limit = Number(upLimit) - 1;
+    if (triggersToEdit[i].scale == DOWN && trigger.actions[0].customData.limit >= upLimit) trigger.actions[0].customData.limit = upLimit - 1;
 
     resp = jelastic.environment.trigger.EditTrigger(ENV_NAME, session, trigger.id, trigger);
     if (resp.result != 0) return resp;
