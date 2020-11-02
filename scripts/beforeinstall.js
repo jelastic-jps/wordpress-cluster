5a31d3e06ba0af7cdf165e36050f86d967426954
sych74 <sych74@gmail.com>
2020-11-02 11:05:20 +0200

    delete tags

--- scripts/beforeinstall.js
+++ scripts/beforeinstall.js
@@ -71,7 +71,6 @@ resp.nodes.push({
 if ('${settings.ls-addon:false}'== 'true') {
   resp.nodes.push({
     nodeType: "litespeedadc",
-    tag: '${settings.bl_tag:2.7}',
     count: ${settings.bl_count:2},
     flexibleCloudlets: ${settings.bl_flexibleCloudlets:8},
     fixedCloudlets: ${settings.bl_fixedCloudlets:1},
