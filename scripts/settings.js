55dcefb98278f8ab26c0d8510f6a00610ceb0c21
vlobzakov <39057925+vlobzakov@users.noreply.github.com>
2020-11-11 09:59:01 +0200

    PoPs removed

--- scripts/settings.js
+++ scripts/settings.js
@@ -5,7 +5,7 @@ import com.hivext.api.core.utils.Transport;
 var cdnAppid = "c05ffa5b45628a2a0c95467ebca8a0b4";
 var lsAppid = "9e6afcf310004ac84060f90ff41a5aba";
 var baseUrl = "https://raw.githubusercontent.com/jelastic-jps/wordpress-cluster/master";
-var cdnText = "Install Lightning-Fast Premium CDN with 130+ PoPs",
+var cdnText = "Install Lightning-Fast Premium CDN",
     sslText = "Install Let's Encrypt SSL with Auto-Renewal";
     lsText = "Install LiteSpeed High-Performance Web Server";
     muText = "Install WordPress Multisite Network";
