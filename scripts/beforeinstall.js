d8d14b7439966af58c0ee5d9f1f1cfc7d05423ce
sych74 <sych74@gmail.com>
2021-02-16 15:22:07 +0200

    Update beforeinstall.js

--- scripts/beforeinstall.js
+++ scripts/beforeinstall.js
@@ -97,7 +97,10 @@ if ('${settings.ls-addon:false}'== 'true') {
       REDIS_ENABLED: "true",
       WAF: "${settings.waf:false}",
       WP_PROTECT: "OFF"
-    }
+    },
+    volumes: [
+      "/var/www/webroot/ROOT"
+    ]
   })
 } else {
   resp.nodes.push({
@@ -123,7 +126,10 @@ if ('${settings.ls-addon:false}'== 'true') {
     env: {
       SERVER_WEBROOT: "/var/www/webroot/ROOT",
       REDIS_ENABLED: "true"
-    }
+    },
+    volumes: [
+      "/var/www/webroot/ROOT"
+    ]
   })
 }
 
