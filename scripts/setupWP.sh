5367a5355b1899c701d52092e827a7f9a0e7457e
sych74 <sych74@gmail.com>
2022-03-10 14:26:37 +0200

    fixed CDN

--- scripts/setupWP.sh
+++ scripts/setupWP.sh
@@ -139,11 +139,9 @@ else
 fi
 
 function generateCdnContent () {
-    echo "wp-content/themes/twentynineteen/style.css" > ~/checkCdnContent.txt;
     echo "wp-includes/css/dist/block-library/style.min.css" >> ~/checkCdnContent.txt;
     echo "wp-includes/css/dist/block-library/theme.min.css" >> ~/checkCdnContent.txt;
     echo "wp-includes/js/wp-embed.min.js" >> ~/checkCdnContent.txt;
-    echo "wp-content/themes/twentynineteen/print.css" >> ~/checkCdnContent.txt;
 }
 
 function checkCdnStatus () {
